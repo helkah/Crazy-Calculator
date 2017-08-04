ed97acf420dce2b4607f873cd7511872b8b94ab7
Helena <helena.halicka@gmail.com>
2017-08-05 00:26:14 +0200

    basic functionality

--- app.js
+++ app.js
@@ -8,27 +8,42 @@ var randomNumber2 = Math.floor((Math.random()*100)+1);
 var operationInputElement = document.getElementById('operation');
 var resultInputElement = document.getElementById('solution');
     
+    
 operationInputElement.value = randomNumber1 + randomOperator + randomNumber2;
 
 var operators = {
     '+':function(a,b){return a+b},
     '-':function(a,b){return a-b},
     '*':function(a,b){return a*b},
-    '/':function(a,b){return a/b},
+    '/':function(a,b){return (Math.floor((a/b)*100))/100},
 }    
     
     
 var result = (operators[randomOperator](randomNumber1,randomNumber2));
+var givenResult = "";    
 
-var buttonsElements = document.getElementsByClassName('charts');
-console.log(buttonsElements); 
+var chartButtons = document.getElementsByClassName('charts');
+var enterButton = document.querySelector("[value='ENTER']");   
 
-    
-buttonsElements.addEventListener('click',function(){
-    
+for (var i=0; i<chartButtons.length; i++){
+        
+    chartButtons[i].addEventListener('click',function(){
+     
+        resultInputElement.value += this.value;
+        givenResult = resultInputElement.value;
+        console.log(givenResult);
+    })
+}
+
+enterButton.addEventListener('click',function(){
+    console.log(result, givenResult);
+    if (result == givenResult){
+        
+        console.log("wow");
+    }else{
+        console.log("wrong");
+    }
 })
-
-
 
 
 
