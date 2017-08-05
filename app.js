343993839c45ffeda42e30788d86a62a34d62642
Helena <helena.halicka@gmail.com>
2017-08-05 23:42:48 +0200

    basic functionality

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 document.addEventListener('DOMContentLoaded',function(){
+
+////////////VARIABLES
     
 var operatorsArray = ["+","-","*","/"];
-var randomOperator = operatorsArray[Math.floor(Math.random() * operatorsArray.length)];
-var randomNumber1 = Math.floor((Math.random()*100)+1);
-var randomNumber2 = Math.floor((Math.random()*100)+1);   
-
-var operationInputElement = document.getElementById('operation');
-var resultInputElement = document.getElementById('solution');
-    
-    
-operationInputElement.value = randomNumber1 + randomOperator + randomNumber2;
-
 var operators = {
     '+':function(a,b){return a+b},
     '-':function(a,b){return a-b},
     '*':function(a,b){return a*b},
-    '/':function(a,b){return (Math.floor((a/b)*100))/100},
-}    
+    '/':function(a,b){return (Math.round((a/b)*100))/100},
+}
+var result = 0;   
+var givenResult = "";
     
-    
-var result = (operators[randomOperator](randomNumber1,randomNumber2));
-var givenResult = "";    
-
+var operationInputElement = document.getElementById('operation');
+var resultInputElement = document.getElementById('solution');
 var chartButtons = document.getElementsByClassName('charts');
-var enterButton = document.querySelector("[value='ENTER']");   
+var enterButton = document.querySelector("[value='ENTER']");  
+    
+function createRandomMathOperation(){    
+    
+    var randomOperator = operatorsArray[Math.floor(Math.random() * operatorsArray.length)];
+    var randomNumber1 = Math.floor((Math.random()*100)+1);
+    var randomNumber2 = Math.floor((Math.random()*100)+1);
+    var rest = randomNumber1%randomNumber2;
+    var helpNumber = randomNumber1 +(randomNumber2-rest);   // to make division without remainders 
+ 
+         
+    if (randomOperator == "/"){
+        if(randomNumber1%randomNumber2 == 0){
+            operationInputElement.value = randomNumber1 + randomOperator + randomNumber2;
+            result = (operators[randomOperator](randomNumber1,randomNumber2));    
+        }else{
+            operationInputElement.value = helpNumber + randomOperator + randomNumber2;
+            result = (operators[randomOperator](helpNumber,randomNumber2));    
+        }
+    }else{
+        operationInputElement.value = randomNumber1 + randomOperator + randomNumber2;
+        result = (operators[randomOperator](randomNumber1,randomNumber2)); 
+    }    
+};
+createRandomMathOperation();    
+    
+    
+    
+///////////EVENTS  
 
 for (var i=0; i<chartButtons.length; i++){
         
@@ -38,10 +58,13 @@ for (var i=0; i<chartButtons.length; i++){
 enterButton.addEventListener('click',function(){
     console.log(result, givenResult);
     if (result == givenResult){
-        
-        console.log("wow");
+         console.log("wow");
+         createRandomMathOperation();
+         resultInputElement.value="";
     }else{
         console.log("wrong");
+        createRandomMathOperation();
+        resultInputElement.value="";
     }
 })
 
