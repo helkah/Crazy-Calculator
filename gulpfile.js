ae48cab019f45de21ff74c4652ef30fbd4e1f06b
Helena <helena.halicka@gmail.com>
2017-09-11 10:38:48 +0200

    sass structure

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'); 
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function() { 
+    return gulp.src('sass/main.scss')
+     .pipe(sourcemaps.init())    
+     .pipe(sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+    })) 
+     .pipe(sourcemaps.write())
+     .pipe(autoprefixer())
+     .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){ 
+   gulp.watch('sass/**/*.scss', ['sass']);
+});
